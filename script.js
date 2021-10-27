7a2612afa7c39adb0ec422ceecd12c3aeeeeb4b5
Rjarmolkovic <90718361+Rjarmolkovic@users.noreply.github.com>
2021-10-27 12:13:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict"
+var tabs = document.getElementsByClassName('tab')
+var tab = document.getElementById(name);
+
+
+function changeTab(target, name){
+  var tabs = document.getElementsByClassName('tab')
+  var tab = document.getElementById(name);
+  var buttons = document.getElementsByTagName("button");
+  for (var i = 0; i < tabs.length; i++) {
+    tabs[i].style.display = "none";
+    tab.style.display = "block";
+    buttons[i].classList.remove("active");
+  }
+
+target.currentTarget.className = "active";
+}
